867ab9aec6c5e03498ff4bf5a34805d877ac9965
hermes7308 <hermes7308@gmail.com>
2020-05-19 21:07:54 -0400

    Add 1. Two Sum

---
+++ leetcode-problems/Solution.cpp
@@ -0,0 +1,39 @@
+#include "Solution.h"
+
+void Solution::runTwoSum()
+{
+	vector<int> nums;
+	nums.reserve(4);
+	nums.push_back(2);
+	nums.push_back(7);
+	nums.push_back(11);
+	nums.push_back(15);
+
+	int target = 9;
+
+	vector<int> result = twoSum(nums, target);
+	cout << result.at(0) << ", " << result.at(1) << endl;
+}
+
+vector<int> Solution::twoSum(vector<int>& nums, int target)
+{
+	for (auto it = nums.begin(); it != nums.end(); it++)
+	{
+		int num = *it;
+		for (auto itx = it + 1; itx != nums.end(); itx++)
+		{
+			int numx = *itx;
+
+			if (target == num + numx)
+			{
+				vector<int> result;
+				result.reserve(2);
+				result.push_back(num);
+				result.push_back(numx);
+
+				return result;
+			}
+		}
+	}
+	return vector<int>();
+}
---
+++ leetcode-problems/Solution.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+	void runTwoSum();
+	vector<int> twoSum(vector<int>& nums, int target);
+};
+
